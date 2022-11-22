26767e35cf61001f5b24e5ba85bbdae786de5e84
shabeertsy <shabeerpptsy@gmail.com>
2022-11-22 19:39:19 +0530

    added button images , points and set 5 points as limit in the game

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
 
 
 
-let data=["rock","paper","scissor"];
+let data = ["rock", "paper", "scissor"];
+let userPoints = 0;
+let sysPoints = 0;
 
-function buttonClick(value){
+// function for button click action
 
-let number=Math.floor(Math.random()*3)
-let sysData=data[number];
+function buttonClick(value) {
 
-document.getElementById('inp-sys').value=sysData;
-let userData=value;
-console.log(userData);
+    let number = Math.floor(Math.random() * 3)
+    let sysData = data[number];
 
-document.getElementById('inp-user').value=userData;
+    document.getElementById('inp-sys').value = sysData;
+    let userData = value;
+    console.log(userData);
+
+    document.getElementById('inp-user').value = userData;
 
 
-if(sysData==userData){
-    document.getElementById('display').innerHTML='Tie'
-}else if(sysData=="scissor"&&userData=="rock"){
-document.getElementById('display').innerHTML='!!! YOU WIN !!!'
-}else if(sysData=="rock"&&userData=="paper"){
-    document.getElementById('display').innerHTML='!!! YOU WIN !!!'
-}else if(sysData=="paper"&&userData=="scissor"){
-    document.getElementById('display').innerHTML='!!! YOU WIN !!!'
-}else if(sysData=="rock"&&userData=="scissor"){
-    document.getElementById('display').innerHTML='!!! SYSTEM WIN !!!'
-}else if(sysData=="paper"&&userData=="rock"){
-    document.getElementById('display').innerHTML='!!! SYSTEM WIN !!!'
-}else if(sysData=="scissor"&&userData=="paper"){
-    document.getElementById('display').innerHTML='!!! SYSTEM WIN !!!'
-}
+    // checking the data and select the winner
+    if (sysData == userData) {
+        document.getElementById('display').innerHTML = 'Tie'
+    } else if (sysData == "scissor" && userData == "rock") {
+        document.getElementById('display').innerHTML = 'YOU WIN'
+        userPoints++;
+    } else if (sysData == "rock" && userData == "paper") {
+        document.getElementById('display').innerHTML = 'YOU WIN'
+        userPoints++;
+    } else if (sysData == "paper" && userData == "scissor") {
+        document.getElementById('display').innerHTML = 'YOU WIN'
+        userPoints++;
+    } else if (sysData == "rock" && userData == "scissor") {
+        document.getElementById('display').innerHTML = 'SYSTEM WIN'
+        sysPoints++;
+    } else if (sysData == "paper" && userData == "rock") {
+        document.getElementById('display').innerHTML = 'SYSTEM WIN '
+        sysPoints++;
+    } else if (sysData == "scissor" && userData == "paper") {
+        document.getElementById('display').innerHTML = 'SYSTEM WIN'
+        sysPoints++;
+    }
+
+    // shows  the points
+    document.getElementById('pts').innerHTML = userPoints;
+    document.getElementById('pts-s').innerHTML = sysPoints;
+
+    // checking the points, choose the  winner and reload
+    if (userPoints == 5 || sysPoints == 5) {
+        if (userPoints == 5) {
+            alert('!!  YOU WIN !!')
+        } else if (sysPoints == 5) {
+            alert('!! SYSTEM WIN !!')
+        }
+        setTimeout(() => {
+            location.reload()
+        }, 2000)
+    }
 }
